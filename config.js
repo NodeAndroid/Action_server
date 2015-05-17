f9568333c6f9cafd0597669628118dc22deb1730
HuangYao <huangyaobyte@gmail.com>
2015-05-17 16:28:44 +0800

    fix config file

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 
 var config = {
   path:"",//root of the project dir;
-  db:"mongodb://192.168.59.103/action",
+  db:"mongodb://localhost/action",
 };
 
+if(process.env.NODE_ENV=='development'){
+  config.db='mongodb://192.168.59.103/action';
+}
 config.path=process.cwd();
 
 module.exports = config;
