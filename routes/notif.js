68e85c8e544a5b5607f88e5f1c369c5d41df3dbc
HuangYao <huangyaobyte@gmail.com>
2015-05-27 17:49:40 +0800

    remove notifivation creatorid

--- routes/notif.js
+++ routes/notif.js
@@ -57,7 +57,7 @@ router.get('/',seHelper.loginRequire,function (req,res,next) {
       aresults.forEach(function(item,index){
         // console.log(item);
         results[index] = results[index].toJSON();
-        delete results[index].creator;
+        delete results[index].send_to;
         if(item[0])
           results[index].send_from_name = item[0].loginname;
         if(item[1])
@@ -103,7 +103,7 @@ router.get('/history',seHelper.loginRequire,function (req,res,next) {
       aresults.forEach(function(item,index){
         // console.log(item);
         results[index] = results[index].toJSON();
-        delete results[index].creator;
+        delete results[index].send_to;
         if(item[0])
           results[index].send_from_name = item[0].loginname;
         if(item[1])
